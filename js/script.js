ba222d6813f87150117a1804642fde04de885e88
Katelyn Booth <katelynbooth@katelyns-mbp.lan>
2020-02-19 11:45:48 -0500

    part two

--- js/script.js
+++ js/script.js
@@ -1,21 +1,31 @@
 let userHealthPoints = 40;
 let grantHealthPoints = 10;
-
 let roundWin = 0;
 
-const startGame = () => {
-    const userName = prompt("What is your name?");
+const getDamage = () => {
+    return Math.floor(Math.random() * 5) + 1;
+}
 
+const startCombat = () => {
+    const userName = prompt("What is your username?");
+    
     while (userHealthPoints > 0 && roundWin < 3) {
-        userHealthPoints = userHealthPoints - (Math.floor(Math.random() * 2) + 1);
-        grantHealthPoints = grantHealthPoints - (Math.floor(Math.random() * 2) + 1);
+        const decideCombat = prompt("Would you like to attack or quit this round?");
 
-        console.log(`${userName} has ${userHealthPoints} health left.`);
-        console.log(`Grant Almighty has ${grantHealthPoints} health left.`);
-        
-        if (grantHealthPoints <= 0) {
-            roundWin++;
-            grantHealthPoints = 10;
+        if (decideCombat.toLowerCase() == "quit") {
+            break;
+
+        } else if (decideCombat.toLowerCase() == "attack"){
+            userHealthPoints = userHealthPoints - getDamage();
+            grantHealthPoints = grantHealthPoints - getDamage();
+    
+            console.log(`${userName} has ${userHealthPoints} health left.`);
+            console.log(`Grant Almighty has ${grantHealthPoints} health left.`);
+    
+            if (grantHealthPoints <= 0) {
+                roundWin++;
+                grantHealthPoints = 10;
+            }
         }
     }
 
@@ -26,8 +36,11 @@ const startGame = () => {
     }
 }
 
-const getUserResponse = prompt("Do you want to play?"); 
-
-if (getUserResponse === "Yes") {
-    startGame();
+const startGame = () => {
+    const getUserResponse = prompt("Do you want to play?"); 
+    if (getUserResponse.toLowerCase() == "yes") {
+    startCombat();
+    }
 }
+
+startGame();
